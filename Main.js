74854a7f018fc3e824697dfd9023d46bba670649
christian <votre.email@example.com>
2026-05-15 15:08:58 +0200

    transformation du portfollion est en un mono page

---
+++ Main.js
@@ -0,0 +1,228 @@
+const themeToggle = document.getElementById("theme-toggle");
+const htmlElement = document.documentElement;
+const resumeBtns = document.querySelectorAll(".resume-btn");
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+
+const navLinks = document.querySelectorAll("header nav a");
+const MenuIcon = document.querySelector("#menu-icon");
+const NavaBar = document.querySelector("header nav");
+
+MenuIcon.addEventListener("click", () => {
+  MenuIcon.classList.toggle("bx-x");
+  NavaBar.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
+    MenuIcon.classList.remove("bx-x");
+    NavaBar.classList.remove("active");
+  });
+});
+
+const currentTheme = localStorage.getItem("theme") || "dark";
+htmlElement.setAttribute("data-theme", currentTheme);
+
+themeToggle.addEventListener("click", () => {
+  const newTheme =
+    htmlElement.getAttribute("data-theme") === "dark" ? "light" : "dark";
+  htmlElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+});
+
+resumeBtns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    const resumeDetails = htmlElement.querySelectorAll(".resume-detail");
+
+    resumeBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+    resumeDetails.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    resumeDetails[idx].classList.add("active");
+  });
+});
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+
+  portfolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  portfolioDetails[index].classList.add("active");
+};
+arrowRight.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRight.classList.add("disabled");
+  }
+  activePortfolio();
+});
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+  activePortfolio();
+});
+
+function updateTextBasedOnSize() {
+  const spans = document.querySelectorAll("h2 span");
+
+  const isMobile =
+    368 >= window.innerWidth <= 468 && 480 >= window.innerHeight <= 642;
+
+  console.log(
+    "Taille de l'écran : ",
+    window.innerWidth,
+    "x",
+    window.innerHeight,
+    " | Mode Mobile :",
+    isMobile
+  );
+
+  spans.forEach((span) => {
+    if (isMobile) {
+      if (
+        span.getAttribute("data-text") === "Elève ingénieur en informatique"
+      ) {
+        span.setAttribute("data-text", "Élève ingénieur IT");
+        span.textContent = "Élève ingénieur IT";
+      } else if (span.getAttribute("data-text") === "Développeur Web junior") {
+        span.setAttribute("data-text", "Dév. Web jr");
+        span.textContent = "Dév. Web jr";
+      } else if (
+        span.getAttribute("data-text") === "Développeur Mobile junior"
+      ) {
+        span.setAttribute("data-text", "Dév. Mobile jr");
+        span.textContent = "Dév. Mobile jr";
+      }
+    } else {
+      if (span.getAttribute("data-text") === "Élève ingénieur IT") {
+        span.setAttribute("data-text", "Elève ingénieur en informatique");
+        span.textContent = "Elève ingénieur en informatique";
+      } else if (span.getAttribute("data-text") === "Développeur Web jr") {
+        span.setAttribute("data-text", "Développeur Web junior");
+        span.textContent = "Développeur Web junior";
+      } else if (span.getAttribute("data-text") === "Développeur Mobile jr") {
+        span.setAttribute("data-text", "Développeur Mobile junior");
+        span.textContent = "Développeur Mobile junior";
+      }
+    }
+  });
+}
+
+window.addEventListener("load", updateTextBasedOnSize);
+window.addEventListener("resize", updateTextBasedOnSize);
+
+//document.addEventListener("DOMContentLoaded", function () {
+//  const downloadButton = document.getElementById("download-cv");
+
+//if (downloadButton) {
+//  downloadButton.addEventListener("click", function (event) {
+//   const cvFile = "assets/cv_de_base.pdf";
+
+//    fetch(cvFile)
+//        .then((response) => {
+//        console.log("Statut de la réponse :", response.status);
+//       if (!response.ok) {
+//          alert(
+//              "⚠️ Le fichier CV n'est pas disponible. Vérifiez le chemin : " +
+//                cvFile
+//            );
+//            event.preventDefault();
+//          }
+//      })
+//        .catch((error) => {
+//         console.error("Erreur de téléchargement :", error);
+//        alert("Une erreur s'est produite lors du téléchargement.");
+//         event.preventDefault();
+//        });
+//  });
+// } else {
+//    console.error("❌ Erreur : Le bouton 'Télécharger mon CV' n'existe pas.");
+//  }
+//});
+
+document
+  .getElementById("rar-download")
+  .addEventListener("click", function (event) {
+    const back_to_the_past = "images/Back_to_the_past.rar";
+
+    fetch(back_to_the_past)
+      .then((response) => {
+        if (!response.ok) {
+          alert("Le fichier est introuvable");
+          event.preventDefault();
+        }
+      })
+      .catch((error) => {
+        console.error("Erreur s'est introuvable:", error);
+        alert("Une erreur est arriver");
+        event.preventDefault();
+      });
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.getElementById("contact-form");
+
+  if (contactForm) {
+    contactForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const formMessage = document.createElement("p");
+      formMessage.id = "form-message";
+      this.appendChild(formMessage);
+
+      const formData = new FormData(this);
+
+      fetch("https://formspree.io/f/xvgzdjad", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            formMessage.textContent = "✅ Message envoyé avec succès !";
+            formMessage.style.color = "green";
+            contactForm.reset();
+          } else {
+            formMessage.textContent =
+              "❌ Une erreur s'est produite. Réessayez.";
+            formMessage.style.color = "red";
+          }
+        })
+        .catch((error) => {
+          console.error("Erreur de soumission :", error);
+          formMessage.textContent =
+            "⚠️ Erreur réseau. Vérifiez votre connexion.";
+          formMessage.style.color = "red";
+        });
+    });
+  } else {
+    console.error("❌ Erreur : Le formulaire 'contact-form' n'existe pas.");
+  }
+});
---
+++ README.md
@@ -0,0 +1,77 @@
+<<<<<<< HEAD
+# 👨‍💻 Portfolio - Alain Datouo
+
+Bienvenue sur mon portfolio de développeur full stack & DevOps !  
+Je suis étudiant ingénieur à l'ESIEA, passionné par le développement web et mobile, l’automatisation, et le cloud computing.
+
+🔗 [Voir le portfolio en ligne](](https://github.com/MBIDA3/portfolio.git)
+
+![Aperçu du portfolio](assets/portfolio.png)
+
+![Aperçu du portfolio](assets/Portfolio2.png)
+
+
+
+---
+
+## 🚀 À propos de moi
+
+Je suis en 3ᵉ année d'école d'ingénieur en informatique, spécialisation **DevOps / Cloud / Full Stack**.  
+J’ai travaillé sur plusieurs projets concrets, incluant des applications mobiles, des dashboards web, et des déploiements automatisés sur AWS.
+
+---
+
+## 🛠️ Compétences techniques
+
+- **Front-end** : Angular, React.js, Vue.js, HTML, CSS, JavaScript  
+- **Back-end** : Node.js, Express.js, Django, Flask, Laravel, SpringBoot, .NET  
+- **Cloud & DevOps** : AWS, Docker, Linux, Terraform, GitHub Actions  
+- **Autres** : Git, Bash, Firebase, Java, Python, C#
+
+---
+
+## 💼 Expérience
+
+### 🔹 Développeur Web & Mobile — LAO Sarl (04/2024 - 07/2024)
+- Application mobile de suivi de pression artérielle
+- Dashboard Angular + API REST
+- Automatisation = -30% de temps de traitement
+- dévelopement d'une API  de gestion des congés cas de LAO sarl
+
+### 🔹 Analyste Fonctionnel — Orange Cameroun (04/2023 - 06/2023)
+- Portail de gestion d’offres télécoms
+- Maquettes Figma + Rédaction de User Stories
+- Analyse métier et entretiens utilisateurs
+
+### 🔹 Développement plateforme web — IME (03/2023 - présent)
+- Site pour le bureau des étudiants de l'université
+
+---
+
+## 🧠 Formation
+
+- 🎓 **ESIEA - Paris** : Ingénierie informatique (DevOps / Cloud / Web)
+- 🎓 **IME  - Douala** : 1re et 2e années en classe préparatoire ingénieur,
+  durant lesquelles j’ai également obtenu un BTS en génie logiciel à l’IME
+   Bonamossadi de Douala.
+
+---
+
+## 🌟 Réalisations clés
+
+- **+30% efficacité** sur traitement de données médicales via automatisation
+- **App QR codes** personnalisée pour 50 entreprises
+- **CI/CD AWS** avec GitHub Actions, Flask, Terraform, EC2, RDS
+
+---
+
+## 📫 Me contacter
+
+- 💼 [LinkedIn](www.linkedin.com/in/christian-embolo-mbida-6549342a9)
+- 💻 [GitHub]([https://github.com/AlainDanp](https://github.com/MBIDA3))
+- ✉️ cmbida38@gmail.com
+
+---
+=======
+# portfolio
+>>>>>>> c8a6160c32d191765db5a50e2c18d9587804d111
